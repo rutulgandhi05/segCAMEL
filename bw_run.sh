9668b53ae04f88bc01fdf2fd25e75f4964cf7600
Rutul Gandhi <rutulgandhirg@gmail.com>
2025-07-04 18:20:01 +0200

    bw_run.sh

---
+++ bw_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J thechosenone
+#SBATCH -o output.txt
+#SBATCH -e error.txt
+#SBATCH -c 8
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 10:00:00 
+#SBATCH -p dev_gpu_a100_il
+#SBATCH --gres=gpu:2
+#SBATCH --mem=64000
+#SBATCH --mail-type ALL
+#SBATCH --mail-user rutul.gandhi@uni-ulm.de
+
+
+
+python -m scripts.feature_extractor
+
+
