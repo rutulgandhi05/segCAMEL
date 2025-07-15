64b5afb70b28842b3ac81e11851de21cbdd7b3d5
Rutul Gandhi <rutulgandhirg@gmail.com>
2025-07-15 15:11:22 +0200

    bw_run_train.sh

--- bw_run_train.sh
+++ bw_run_train.sh
@@ -7,14 +7,13 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -t 00:30:00 
-#SBATCH -p dev_gpu_a100_il
+#SBATCH -p gpu_a100_il
 #SBATCH --gres=gpu:2
 #SBATCH --mem=64000
 #SBATCH --mail-type ALL
 #SBATCH --mail-user rutul.gandhi@uni-ulm.de
 
 source venv/bin/activate
-python ../flash-attention/hopper/setup.py install
 python -m scripts.train
 
 
