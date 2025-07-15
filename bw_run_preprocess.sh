edf30ac58e0c33ac37efcb40f9d6b44b4d795cdc
Rutul Gandhi <rutulgandhirg@gmail.com>
2025-07-15 09:48:29 +0200

    run for preprocess and train added

---
+++ bw_run_preprocess.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -J SegCAMEL
+#SBATCH -o preprocess_output.txt
+#SBATCH -e preprocess_error.txt
+#SBATCH -c 8
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 00:30:00 
+#SBATCH -p dev_gpu_a100_il
+#SBATCH --gres=gpu:2
+#SBATCH --mem=64000
+#SBATCH --mail-type ALL
+#SBATCH --mail-user rutul.gandhi@uni-ulm.de
+
+source venv/bin/activate
+python -m scripts.preprocess
+
+
--- bw_run_train.sh
+++ bw_run_train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH -J SegCAMEL
-#SBATCH -o output.txt
-#SBATCH -e error.txt
+#SBATCH -o train_output.txt
+#SBATCH -e train_error.txt
 #SBATCH -c 8
 #SBATCH -N 1
 #SBATCH -n 1
@@ -14,6 +14,6 @@
 #SBATCH --mail-user rutul.gandhi@uni-ulm.de
 
 source venv/bin/activate
-python -m scripts.preprocess
+python -m scripts.train
 
 
