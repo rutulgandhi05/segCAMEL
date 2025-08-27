680c6b9d0bae3fea6211f185b8beaaf61943c091
Rutul Gandhi <rutulgandhirg@gmail.com>
2025-08-28 01:47:12 +0200

    train hercules

--- bw_run_pipeline.sh
+++ bw_run_pipeline.sh
@@ -17,7 +17,7 @@ module load devel/cuda/12.8
 
 export HERCULES_DATASET=$(ws_find hercules_dataset)
 export TMP_HERCULES_DATASET=$TMPDIR/segcamel/hercules_dataset
-
+mkdir -p $TMP_HERCULES_DATASET
 echo "[INFO] Copying dataset to $TMP_HERCULES_DATASET"
 cp -r --verbose $HERCULES_DATASET/* $TMP_HERCULES_DATASET/
 echo "[INFO] Dataset copied."
