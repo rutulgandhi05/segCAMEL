178fa1d6ed64efd3de7abb16d80d4b5fad9b3577
Rutul Gandhi <rutulgandhirg@gmail.com>
2025-09-22 04:17:15 +0200

    inference

--- bw_run_infernce.sh
+++ bw_run_infernce.sh
@@ -19,7 +19,7 @@ export HERCULES_DATASET=$(ws_find hercules_dataset_complete)
 export TMP_HERCULES_DATASET=$TMPDIR/segcamel/hercules_dataset
 mkdir -p $TMP_HERCULES_DATASET
 
-export PREPROCESS_FOLDERS="stream_01_Day"
+export PREPROCESS_FOLDERS="street_01_Day"
 echo "[INFO] Copying $PREPROCESS_FOLDERS to $TMP_HERCULES_DATASET"
 cp -r  $HERCULES_DATASET/$PREPROCESS_FOLDERS $TMP_HERCULES_DATASET/
 ls $TMP_HERCULES_DATASET
@@ -29,19 +29,19 @@ export HERCULES_PROCESSED=$(ws_find hercules_preprocessed)
 export PREPROCESS_OUTPUT_DIR=$TMPDIR/segcamel/processed_data
 export TRAIN_CHECKPOINTS=$TMPDIR/segcamel/checkpoints
 export FEAT_MODE="rvi"  # "rvi", "rv", "none", etc.
-export INFERENCE_OUTPUT_DIR=$TMPDIR/segcamel/$(date +"%d%m%Y_%H%M")_inference_output_stream_01_Day_$FEAT_MODE
-export SEGMENTATION_OUT_DIR=$TMPDIR/segcamel/$(date +"%d%m%Y_%H%M")_unsup_outputs_stream_01_Day_$FEAT_MODE
+export INFERENCE_OUTPUT_DIR=$TMPDIR/segcamel/$(date +"%d%m%Y_%H%M")_inference_output_street_01_Day_$FEAT_MODE
+export SEGMENTATION_OUT_DIR=$TMPDIR/segcamel/$(date +"%d%m%Y_%H%M")_unsup_outputs_street_01_Day_$FEAT_MODE
 export PIPELINE_MODE="inference"
 
 mkdir -p $TRAIN_CHECKPOINTS
 mkdir -p $INFERENCE_OUTPUT_DIR
-cp --verbose $HERCULES_PROCESSED/11092025_1205_segcamel_train_output_epoch_50_$FEAT_MODE/checkpoints/best_model.pth $TRAIN_CHECKPOINTS/
+cp --verbose $HERCULES_PROCESSED/22092025_0320_segcamel_train_output_epoch_50_$FEAT_MODE/checkpoints/best_model.pth $TRAIN_CHECKPOINTS/
 export TRAIN_CHECKPOINT_PTH=$TRAIN_CHECKPOINTS/best_model.pth
 
 python -m scripts.preprocess
 python -m scripts.segment_once
 
-cp -r $INFERENCE_OUTPUT_DIR/ $HERCULES_PROCESSED/11092025_1205_segcamel_train_output_epoch_50_$FEAT_MODE/
-cp -r $SEGMENTATION_OUT_DIR/ $HERCULES_PROCESSED/11092025_1205_segcamel_train_output_epoch_50_$FEAT_MODE/
+cp -r $INFERENCE_OUTPUT_DIR/ $HERCULES_PROCESSED/22092025_0320_segcamel_train_output_epoch_50_$FEAT_MODE/
+cp -r $SEGMENTATION_OUT_DIR/ $HERCULES_PROCESSED/22092025_0320_segcamel_train_output_epoch_50_$FEAT_MODE/
 
 echo "[INFO] Job completed successfully."
